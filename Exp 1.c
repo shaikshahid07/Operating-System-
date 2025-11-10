e2c61985af13137a54c966665a5403e0b04c1783
SHAIK SHAHID <192424311.simats@saveetha.com>
2025-11-10 16:45:27 +0530

    Exp 1.c

---
+++ Exp 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid = fork();
+    if (pid == 0)
+        printf("Child Process: PID=%d, PPID=%d\n", getpid(), getppid());
+    else
+        printf("Parent Process: PID=%d, PPID=%d\n", getpid(), getppid());
+    return 0;
+}
