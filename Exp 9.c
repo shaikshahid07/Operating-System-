1e29cf4c11ec8a167cfbf338b02cf3a8b607d039
SHAIK SHAHID <192424311.simats@saveetha.com>
2025-11-11 12:32:06 +0530

    Exp 9.c

---
+++ Exp 9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+
+int main() {
+    key_t key = 1234;
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+    char *str = (char*) shmat(shmid, NULL, 0);
+
+    printf("Write Data: ");
+    fgets(str, 100, stdin);
+
+    printf("Data written in memory: %s\n", str);
+    shmdt(str);
+    return 0;
+}
